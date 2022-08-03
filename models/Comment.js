b65ad83083d016c5f6fc9fff883595c8f8d3423a
Chris <chrismanfredijr@gmail.com>
2022-08-02 21:14:29 -0400

    subdocuments and custom id models

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,65 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model, Types } = require('mongoose');
 
-const CommentSchema = new Schema({
-    writtenBy: {
+const CommentSchema = new Schema(
+    {
+      writtenBy: {
         type: String
-    },
-    commentBody: {
-        type:String
-    },
-    createdAt: {
+      },
+      commentBody: {
+        type: String
+      },
+      createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+      // use ReplySchema to validate data for a reply
+      replies: [ReplySchema]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
     }
-});
+  );
+
+  CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+  });
+
+const ReplySchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent comment's _id field
+      replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      replyBody: {
+        type: String
+      },
+      writtenBy: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      }
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      }
+    }
+  );
+
+  PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  });
+
 
 const Comment = model('Comment', CommentSchema);
 
