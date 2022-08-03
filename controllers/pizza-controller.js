326f07ad6321a33a4d20aa29b0b02e7034f7d4dc
Chris <chrismanfredijr@gmail.com>
2022-08-02 22:55:35 -0400

    indexdb fixes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -16,7 +16,18 @@ const pizzaController = {
         res.sendStatus(400);
       });
   },
-
+  // update pizza by id
+updatePizza({ params, body }, res) {
+  Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => res.status(400).json(err));
+},
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
